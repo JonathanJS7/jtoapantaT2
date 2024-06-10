52230d6cad6de958235aa2fdd004cdb0bfa382f3
JonathanJS7 <jonathan_js7@hotmail.com>
2024-06-09 23:29:51 -0500

    Listo

--- Views/vPrincipal.xaml.cs
+++ Views/vPrincipal.xaml.cs
@@ -11,6 +11,7 @@ public partial class vPrincipal : ContentPage
     {
         if (pkEstudiantes.SelectedIndex < 0)
             DisplayAlert("Error", "No hay seleccion de estudiantes", "Ok");
+
         else
         {
             string datos = pkEstudiantes.Items[pkEstudiantes.SelectedIndex].ToString();
@@ -18,21 +19,42 @@ public partial class vPrincipal : ContentPage
             double notaSeg1 = Convert.ToDouble(txtSeg1.Text);
             double notaExa1 = Convert.ToDouble(txtExa1.Text);
 
-            double promedioParcial1 = (notaSeg1 + notaExa1) * 0.5;
+            double promedioParcial1 = (notaSeg1*0.30 + notaExa1*0.20);
 
             double notaSeg2 = Convert.ToDouble(txtSeg2.Text);
             double notaExa2 = Convert.ToDouble(txtExa2.Text);
 
-            double promedioParcial2 = (notaSeg2 + notaExa2) * 0.5;
+            double promedioParcial2 = (notaSeg2*0.30 + notaExa2*0.20);
 
-            double promedioFinal = (promedioParcial1 + promedioParcial2) / 2;
+            double promedioFinal = (promedioParcial1 + promedioParcial2);
+            string estado;
 
-            DisplayAlert("Estudiante"+datos, "La nota final es: " + promedioFinal.ToString(), "Aceptar");
+            if (promedioFinal >= 7)
+            {
+                estado = "APROBADO";
+            } else if (promedioFinal >= 5 && promedioFinal < 7)
+                {
+                estado = "COMPLEMENTARIO";
+                } else
+                    {
+                    estado = "REPROBADO";
+                    }
+            string fecha = dpFecha.Date.ToString("dd/MM/yyyy");
 
+            txtSeg1.Text = "";
+            txtExa1.Text = "";
+            txtSeg2.Text = "";
+            txtExa2.Text = "";
+            pkEstudiantes.SelectedIndex = -1;
+
+            DisplayAlert("Estudiante "+datos,
+                "La nota final es: " + promedioFinal.ToString("F2") 
+                +"\nEstado: " + estado+
+                "\nfecha: "+ fecha,
+                "\nAceptar");
 
         }
 
-        string fecha = dpFecha.Date.ToString();
-        DisplayAlert("ALERTA", "La fecha seleccionada es: " + fecha, "Ok");
+
     }
 }
